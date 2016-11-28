5d56054998518a0d10cd49ba554ba5532597f7d4
Arber <arber.qoku@outlook.com>
2016-11-28 11:53:52 +0100

    Hi

--- app/src/main/java/com/flatshare/flatshare/MainActivity.java
+++ app/src/main/java/com/flatshare/flatshare/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     //Test David
+    //Test Arber
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
