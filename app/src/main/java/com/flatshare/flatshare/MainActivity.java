3771f384568bb77c9af1f0dd791bd4a976e7ddda
Sandro <sandro-gauss@web.de>
2016-11-28 12:06:55 +0100

    test sandro

--- app/src/main/java/com/flatshare/flatshare/MainActivity.java
+++ app/src/main/java/com/flatshare/flatshare/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends AppCompatActivity {
 
     //Test David
     //Test Arber
-    //
+    //Sandro
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
