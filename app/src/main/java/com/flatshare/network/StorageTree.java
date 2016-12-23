88b8a44f814759c7a80ad2d7a7db0661d446135b
Arber <arber.qoku@outlook.com>
2016-12-23 12:46:12 +0100

    added StorageTree (containing all paths for storage)

---
+++ app/src/main/java/com/flatshare/network/StorageTree.java
@@ -0,0 +1,17 @@
+package com.flatshare.network;
+
+/**
+ * Created by Arber on 21.12.2016.
+ */
+
+public interface StorageTree {
+
+    String URL = "gs://flatshare-5d4c6.appspot.com";
+
+    String TENANT_PATH = "tenants/";
+    String APARTMENT_PATH = "apartments/";
+
+    String IMAGES_PATH = "images/";
+    String VIDEOS_PATH = "videos/";
+
+}
