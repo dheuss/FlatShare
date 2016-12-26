80fc38009ac2affa1063b4b34420fd43edb48625
Arber <arber.qoku@outlook.com>
2016-12-26 12:49:51 +0100

    databasePath, storagePath finished

---
+++ app/src/main/java/com/flatshare/domain/state/UserState.java
@@ -0,0 +1,16 @@
+package com.flatshare.domain.state;
+
+/**
+ * TODO: maybe later!
+ * Created by Arber on 26/12/2016.
+ */
+
+public class UserState {
+
+    private int classificationId;
+    private String tenantId;
+    private String apartmentId;
+
+
+
+}
