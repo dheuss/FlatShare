925fadb672871974d8e6cab2c78a2436d6aa4b42
David Heuss <heussd@cip.ifi.lmu.de>
2017-01-02 19:49:17 +0100

    SettingsActivity started

--- app/src/main/java/com/flatshare/presentation/ui/activities/SettingsActivity.java
+++ app/src/main/java/com/flatshare/presentation/ui/activities/SettingsActivity.java
@@ -1,12 +1,13 @@
 package com.flatshare.presentation.ui.activities;
 
 import android.content.Intent;
-import android.media.Image;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ProgressBar;
 
 import com.flatshare.R;
 import com.flatshare.presentation.presenters.SettingsPresenter;
@@ -16,7 +17,24 @@ import com.flatshare.threading.MainThreadImpl;
 
 public class SettingsActivity extends AbstractActivity implements SettingsPresenter.View {
 
-    private ImageButton profilSettingsBackButton;
+    private Button btnChangeEmail;
+    private Button btnChangePassword;
+    private Button btnSendResetEmail;
+    private Button btnRemoveUser;
+    private Button changeEmail;
+    private Button changePassword;
+    private Button sendEmail;
+    private Button remove;
+    private Button signOut;
+
+    private EditText oldEmail;
+    private EditText newEmail;
+    private EditText password;
+    private EditText newPassword;
+
+    private ProgressBar progressBar;
+
+    private ImageButton back;
 
     private static final String TAG = "SettingsActivity";
 
@@ -35,9 +53,55 @@ public class SettingsActivity extends AbstractActivity implements SettingsPresen
                 this
         );
 
-        profilSettingsBackButton.setOnClickListener(new View.OnClickListener(){
+        if (progressBar != null) {
+            progressBar.setVisibility(View.GONE);
+        }
+
+        btnChangeEmail.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
+                oldEmail.setVisibility(View.GONE);
+                newEmail.setVisibility(View.VISIBLE);
+                password.setVisibility(View.GONE);
+                newPassword.setVisibility(View.GONE);
+                changeEmail.setVisibility(View.VISIBLE);
+                changePassword.setVisibility(View.GONE);
+                sendEmail.setVisibility(View.GONE);
+                remove.setVisibility(View.GONE);
+            }
+        });
+
+        btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                oldEmail.setVisibility(View.GONE);
+                newEmail.setVisibility(View.GONE);
+                password.setVisibility(View.GONE);
+                newPassword.setVisibility(View.VISIBLE);
+                changeEmail.setVisibility(View.GONE);
+                changePassword.setVisibility(View.VISIBLE);
+                sendEmail.setVisibility(View.GONE);
+                remove.setVisibility(View.GONE);
+            }
+        });
+
+        btnSendResetEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                oldEmail.setVisibility(View.VISIBLE);
+                newEmail.setVisibility(View.GONE);
+                password.setVisibility(View.GONE);
+                newPassword.setVisibility(View.GONE);
+                changeEmail.setVisibility(View.GONE);
+                changePassword.setVisibility(View.GONE);
+                sendEmail.setVisibility(View.VISIBLE);
+                remove.setVisibility(View.GONE);
+            }
+        });
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
                 startActivity(new Intent(SettingsActivity.this, ProfilSettingsActivity.class));
             }
         });
@@ -45,14 +109,42 @@ public class SettingsActivity extends AbstractActivity implements SettingsPresen
 
     @Override
     protected int getLayoutResourceId() {
-        return R.layout.activity_settings;    }
+        return R.layout.activity_settings;
+    }
 
     private void bindView(){
-        profilSettingsBackButton = (ImageButton)findViewById(R.id.settingsPorilfBackBtn);
+        btnChangeEmail = (Button) findViewById(R.id.change_email_button);
+        btnChangePassword = (Button) findViewById(R.id.change_password_button);
+        btnSendResetEmail = (Button) findViewById(R.id.sending_pass_reset_button);
+        btnRemoveUser = (Button) findViewById(R.id.remove_user_button);
+        changeEmail = (Button) findViewById(R.id.changeEmail);
+        changePassword = (Button) findViewById(R.id.changePass);
+        sendEmail = (Button) findViewById(R.id.send);
+        remove = (Button) findViewById(R.id.remove);
+        signOut = (Button) findViewById(R.id.sign_out);
+
+        oldEmail = (EditText) findViewById(R.id.old_email);
+        newEmail = (EditText) findViewById(R.id.new_email);
+        password = (EditText) findViewById(R.id.password);
+        newPassword = (EditText) findViewById(R.id.newPassword);
+
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        back = (ImageButton)findViewById(R.id.settings_profil_back_button);
+
+        oldEmail.setVisibility(View.GONE);
+        newEmail.setVisibility(View.GONE);
+        password.setVisibility(View.GONE);
+        newPassword.setVisibility(View.GONE);
+        changeEmail.setVisibility(View.GONE);
+        changePassword.setVisibility(View.GONE);
+        sendEmail.setVisibility(View.GONE);
+        remove.setVisibility(View.GONE);
     }
 
     @Override
     public void showError(String message) {
 
     }
+
 }
