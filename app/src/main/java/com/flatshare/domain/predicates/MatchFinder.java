f60fb5d89f004b680ac731b0386842d9dc5377c7
Arber <arber.qoku@outlook.com>
2017-01-03 22:07:15 +0100

    added missing predicates

--- app/src/main/java/com/flatshare/domain/predicates/MatchFinder.java
+++ app/src/main/java/com/flatshare/domain/predicates/MatchFinder.java
@@ -13,12 +13,12 @@ class MatchFinder {
 
     public boolean apMatchesTenantFilter(ApartmentUserProfile a, TenantFilterSettings tf) {
         return (a.getArea() >= tf.getAreaFrom() && a.getArea() <= tf.getAreaTo())
-                && (a.getPrice() >= tf.getPriceFrom() && a.getPrice() <= tf.getPriceTo());
-//                && ((tf.getPetsAllowed() == 2) || (!a.hasPets() && tf.getPetsAllowed() == 0) || (a.hasPets() && tf.getPetsAllowed() == 1))
-//                && ((tf.wantsInternet() == 2) || (!a.hasInternet() && tf.wantsInternet() == 0) || (a.hasInternet() && tf.wantsInternet() == 1))
-//                && ((tf.wantsWashingMachine() == 2) || (!a.hasWashingMachine() && tf.wantsPurposeApartment() == 0) || (a.hasWashingMachine() && tf.wantsPurposeApartment() == 1))
-//                && ((tf.wantsSmokerApartment() == 2) || (!a.isSmokerApartment() && tf.wantsSmokerApartment() == 0) || (a.isSmokerApartment() && tf.wantsSmokerApartment() == 1))
-//                && ((tf.wantsPurposeApartment() == 2) || (!a.isPurposeApartment() && tf.wantsPurposeApartment() == 0) || (a.isPurposeApartment() && tf.wantsPurposeApartment() == 1));
+                && (a.getPrice() >= tf.getPriceFrom() && a.getPrice() <= tf.getPriceTo())
+                && ((tf.getPetsAllowed() == 2) || (!a.hasPets() && tf.getPetsAllowed() == 0) || (a.hasPets() && tf.getPetsAllowed() == 1))
+                && ((tf.wantsInternet() == 2) || (!a.hasInternet() && tf.wantsInternet() == 0) || (a.hasInternet() && tf.wantsInternet() == 1))
+                && ((tf.wantsWashingMachine() == 2) || (!a.hasWashingMachine() && tf.wantsPurposeApartment() == 0) || (a.hasWashingMachine() && tf.wantsPurposeApartment() == 1))
+                && ((tf.wantsSmokerApartment() == 2) || (!a.isSmokerApartment() && tf.wantsSmokerApartment() == 0) || (a.isSmokerApartment() && tf.wantsSmokerApartment() == 1))
+                && ((tf.wantsPurposeApartment() == 2) || (!a.isPurposeApartment() && tf.wantsPurposeApartment() == 0) || (a.isPurposeApartment() && tf.wantsPurposeApartment() == 1));
     }
 
     public boolean tenantMatchesApFilter(TenantUserProfile t, ApartmentFilterSettings af) {
