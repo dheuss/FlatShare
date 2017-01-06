42749661e9206dc7cc70cec412c1657f889736af
Arber <arber.qoku@outlook.com>
2017-01-06 14:36:38 +0100

    ChatPresenter added

---
+++ app/src/main/java/com/flatshare/presentation/presenters/chat/ChatPresenter.java
@@ -0,0 +1,14 @@
+package com.flatshare.presentation.presenters.chat;
+
+import com.flatshare.presentation.presenters.base.BasePresenter;
+import com.flatshare.presentation.ui.BaseView;
+
+/**
+ * Created by david on 28.12.2016.
+ */
+
+public interface ChatPresenter extends BasePresenter {
+    interface View extends BaseView{
+
+    }
+}
