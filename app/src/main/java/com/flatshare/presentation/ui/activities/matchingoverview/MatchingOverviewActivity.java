43b0971c8615f269301334a11d378383b0d1da62
David Heuss <heussd@cip.ifi.lmu.de>
2017-01-09 09:53:48 +0100

    two new activities for MatchingOverview and MatchingInfo

---
+++ app/src/main/java/com/flatshare/presentation/ui/activities/matchingoverview/MatchingOverviewActivity.java
@@ -0,0 +1,15 @@
+package com.flatshare.presentation.ui.activities.matchingoverview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.flatshare.R;
+
+public class MatchingOverviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matching_overview);
+    }
+}
---
+++ app/src/main/java/com/flatshare/presentation/ui/activities/matchingoverview/matchinginfo/MatchingInfoActivity.java
@@ -0,0 +1,15 @@
+package com.flatshare.presentation.ui.activities.matchingoverview.matchinginfo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.flatshare.R;
+
+public class MatchingInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matching_info);
+    }
+}
