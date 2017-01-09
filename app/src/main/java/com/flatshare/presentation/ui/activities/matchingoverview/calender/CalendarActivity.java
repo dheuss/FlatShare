2105f666b96a5efd0ffa894c90784a05124afbc6
Sandro <sandro-gauss@web.de>
2017-01-09 13:31:32 +0100

    mein alten stand hergestellt

--- app/src/main/java/com/flatshare/presentation/ui/activities/matchingoverview/calender/CalendarActivity.java
+++ app/src/main/java/com/flatshare/presentation/ui/activities/matchingoverview/calender/CalendarActivity.java
@@ -1,10 +1,14 @@
 package com.flatshare.presentation.ui.activities.matchingoverview.calender;
 
 
+
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CalendarView;
 import android.widget.ImageButton;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.flatshare.R;
@@ -14,10 +18,18 @@ import com.flatshare.presentation.ui.AbstractActivity;
 import com.flatshare.presentation.ui.activities.matching.MatchingActivity;
 import com.flatshare.threading.MainThreadImpl;
 
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class CalendarActivity extends AbstractActivity implements CalendarPresenter.View {
 
     private CalendarPresenter mPresenter;
+    private CalendarView calendarView;
     private ImageButton couchChatButton;
+    private TextView dateDisplay;
+    List<Long> dateList = new ArrayList<Long>();
+
     private static final String TAG = "CalenderAct";
 
     @Override
@@ -34,12 +46,39 @@ public class CalendarActivity extends AbstractActivity implements CalendarPresen
                 startActivity(new Intent(CalendarActivity.this, MatchingActivity.class));
             }
         });
+
+
+        calendarView.setFirstDayOfWeek(2);
+
+        calendarView.setMinDate(System.currentTimeMillis() - 1000);
+
+        dateDisplay.setText("Date: ");
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int year, int month, int day) {
+
+                //for (int i = 0; i <= dateList.size(); i++){
+                //    if (dateList.get(i) != calendarView.getDate()){
+                        dateList.add(calendarView.getDate());
+                        //calendarView.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+                //    }
+                //}
+
+                dateDisplay.setText("Date: " + day + " / " + month + " / " + year);
+
+                Toast.makeText(getApplicationContext(), "Selected Date:\n" + "Day = " + day + "\n" + "Month = " + month + "\n" + "Year = " + year, Toast.LENGTH_LONG).show();
+            }
+        });
     }
 
 
     private void bindView() {
         couchChatButton = (ImageButton)findViewById(R.id.couchChatBtn);
+        calendarView = (CalendarView) findViewById(R.id.calendarView);
+        dateDisplay = (TextView) findViewById(R.id.date_display);
     }
+
 
     @Override
     protected int getLayoutResourceId() {
