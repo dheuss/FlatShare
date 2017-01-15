5ac6fddaec341dd5c9a83902704e40fa38db6ea1
David Heuss <heussd@cip.ifi.lmu.de>
2017-01-15 18:09:41 +0100

    MatchigOverview

--- app/src/main/java/com/flatshare/presentation/ui/activities/matchingoverview/MatchingOverviewActivity.java
+++ app/src/main/java/com/flatshare/presentation/ui/activities/matchingoverview/MatchingOverviewActivity.java
@@ -1,20 +1,25 @@
 package com.flatshare.presentation.ui.activities.matchingoverview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ProgressBar;
 
 import com.flatshare.R;
 import com.flatshare.presentation.presenters.matchingoverview.MatchingOverviewPresenter;
 import com.flatshare.presentation.presenters.matchingoverview.impl.MatchingOverviewPresenterImpl;
 import com.flatshare.presentation.ui.AbstarctFragmentAcivity;
+import com.flatshare.presentation.ui.activities.matchingoverview.calendar.CalendarActivity;
 import com.flatshare.threading.MainThreadImpl;
 
 public class MatchingOverviewActivity extends AbstarctFragmentAcivity implements MatchingOverviewPresenter.View {
+
+    private Button changeToCalendarButton;
 
     private ProgressBar progressBar;
 
@@ -44,11 +49,18 @@ public class MatchingOverviewActivity extends AbstarctFragmentAcivity implements
             progressBar.setVisibility(View.GONE);
         }
 
+        changeToCalendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getActivity(), CalendarActivity.class));
+            }
+        });
+
         return view;
     }
 
     public void bindView(View view){
-
+        changeToCalendarButton = (Button)view.findViewById(R.id.change_to_calendar_button);
     }
 
     @Override
