4178e1d338b74a62451b976b2ae54bf90c6f89bf
heussd <equino490>
2017-01-18 11:49:32 +0100

    !!!SettingsActivity is finished!!!

--- app/src/main/java/com/flatshare/presentation/ui/activities/settings/SettingsActivity.java
+++ app/src/main/java/com/flatshare/presentation/ui/activities/settings/SettingsActivity.java
@@ -1,23 +1,17 @@
 package com.flatshare.presentation.ui.activities.settings;
 
 import android.app.AlertDialog;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.PopupWindow;
 import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
-import android.view.ViewGroup.LayoutParams;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.flatshare.R;
 import com.flatshare.presentation.presenters.settings.SettingsPresenter;
