8841c9ccf5b3afe5be94c6d5de4f26f71ef58c68
Sandro <sandro-gauss@web.de>
2017-01-22 20:41:44 +0100

    matchingoverview zwischenstand

--- app/src/main/java/com/flatshare/presentation/ui/activities/matchingoverview/MatchingOverviewActivity.java
+++ app/src/main/java/com/flatshare/presentation/ui/activities/matchingoverview/MatchingOverviewActivity.java
@@ -14,6 +14,7 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 import com.flatshare.R;
+
 import com.flatshare.presentation.presenters.matchingoverview.MatchingOverviewPresenter;
 import com.flatshare.presentation.presenters.matchingoverview.impl.MatchingOverviewPresenterImpl;
 import com.flatshare.presentation.ui.AbstractFragmentActivity;
